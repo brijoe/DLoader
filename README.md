db3177b650026362f2373579ad07e6ca44739fe2
bridgeliang <bridge.lang@uxin.com>
2018-05-16 22:04:22 +0800

    更新库。

--- README.md
+++ README.md
@@ -1,52 +1,35 @@
-#ImageLoader
+# DLoader
 
-Android本地、网络图片加载库。
-
-## 使用
-
-下载[jar](jar/imageloader.jar)包，复制到项目的libs下，然后添加依赖。
-
-任何需要的地方调用以下代码
-
-```java
- ImageLoader.with(context).load(url, imageview);
-```
-注意权限：
-
-```xml
- <uses-permission android:name="android.permission.INTERNET"></uses-permission>
- <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"></uses-permission>
-
-```
-
-## 效果图
-
-### 关于Sample
-
-* Android Studio 用户，以module方式导入[sample/sample-imageloader-for-androidstudio](sample/sample-imageloader-for-androidstudio) 。
-* Eclipse用户，以项目的方式，导入[sample/sample-imageloader-for-eclipse](sample/sample-imageloader-for-eclipse) 。
-
-### 加载网络图片
-
-<img src="imageloader_01.gif"  width="320px"/>
-
-### 加载本地图片
-
-<img src="imageloader_02.png"  width="320px"/>
-
-<img src="imageloader_03.png"  width="320px"/>
+简易的Android网络图片加载库，适用于较小规模的网络图片加载需求，仅供学习和研究。
 
 ## 特性
 
 * 支持LIFO，FIFO 加载图片策略
 * 支持内存缓存、硬盘缓存
 * 支持网络图片加载
-* 支持本地图片加载
-* 支持file、drawable、assets、content等Schema图片的加载
 
-##License
-Copyright 2016 Bridge George
 
+## 使用
+
+任何需要的地方调用以下代码
+
+```
+ DLoader.with(context).load(url, imageview);
+```
+注意权限：
+
+```
+ <uses-permission android:name="android.permission.INTERNET" />
+ <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+
+```
+
+## Demo 效果
+
+<img src="images/screenshot.jpg" width=250/>
+
+
+## License
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
---
+++ library/src/main/java/io/github/brijoe/ImageSampler.java
@@ -0,0 +1,98 @@
+package io.github.brijoe;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import java.io.FileDescriptor;
+
+/**
+ * 提供了图片压缩方法和从不同源中加载Bitmap的方法
+ */
+class ImageSampler {
+    private static final String TAG = "ImageResizer";
+
+    public ImageSampler() {
+    }
+
+    /**
+     * 根据请求的尺寸从资源文件中载入Bitmap
+     *
+     * @param res
+     * @param resId
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
+
+    public Bitmap decodeSampleBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
+        //First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+        //Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        //Decode bitmap width inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    /**
+     * 根据请求的bitmap 尺寸 从文件描述中载入bitmap
+     *
+     * @param fd
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
+
+    public Bitmap decodeSampleBitmapFromFileDescriptor(FileDescriptor fd, int reqWidth, int reqHeight) {
+        //First decode width inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFileDescriptor(fd, null, options);
+        //Calculate inSampleSize
+
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        //Decode bitmap width inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFileDescriptor(fd, null, options);
+
+
+    }
+
+    /**
+     * 根据请求尺寸 计算 inSampleSize 参数
+     * inSampleSize 参数指定了宽和高的缩放比例 例如 inSampleSize=1 那么就是原始尺寸
+     * inSampleSize=2 那么 宽高就是 1/2  整体像素数减少 到1/4
+     *
+     * @param options
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
+
+    private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        //0 代表 不进行inSample 计算压缩
+        if (reqWidth == 0 || reqHeight == 0) {
+            return 1;
+        }
+        //Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        Log.d(TAG, "origin,w=" + width + " h=" + height);
+        int inSampleSize = 1;
+        if (height > reqHeight || width > reqWidth) {
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+            //Calculate the largest inSampleSize value that is a power of 2 and keep
+            //both height adn width larger than the requested height ad width.
+            while ((halfHeight / inSampleSize) >= reqHeight && (halfWidth / inSampleSize >= reqWidth)) {
+                inSampleSize *= 2;
+            }
+        }
+        Log.d(TAG, "sampleSize: " + inSampleSize);
+        return inSampleSize;
+    }
+}
